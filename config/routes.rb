1f8aed4114d235bbd0cd3ea35a5125016cbcb3ae
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-08 16:43:06 +0100

    create routes users, goats, reserves and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users, except [:index, :new, :create]
+  resources :goats do
+    resources :reserves
+    resources :reviews, only [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
