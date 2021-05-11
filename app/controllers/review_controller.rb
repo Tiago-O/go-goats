ee6c6ca71dc6e82d076d4f8900b04c13261c8cbb
Diogo Broco <dpintobroco@gmail.com>
2021-05-11 20:36:59 +0100

    Added review controller with create method

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,5 +1,14 @@
 class ReviewController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @goat = Goat.find(params[:goat_id])
+    @user = User.find(params[:user_id])
+    @review.goat = @goat
+    @review.user = @user
 
+    @review.save
+    redirect_to goat_path
+  end
 
   private
 
