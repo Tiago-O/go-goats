a652ef207a2ecbe1ffbd7e8c4581e5ca3ee93aa5
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-11 18:51:56 +0100

    routes corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, except [:index, :new, :create]
+  resources :users, except: [:index, :new, :create]
   resources :goats do
     resources :reserves
-    resources :reviews, only [:create]
+    resources :reviews, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
