ad2bb5f03862b6eb39cdfe891345b3f590d94529
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-11 20:14:43 +0100

    goats controller done

---
+++ app/controllers/goats_controller.rb
@@ -0,0 +1,44 @@
+class GoatsController < ApplicationController
+  before_action :set_goat, only: %i[show edit update destroy]
+
+  def index
+    @goats = Goat.all
+  end
+
+  def show; end
+
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+    if @goat.save
+      redirect_to @goat
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @goat.update(goat_params)
+    redirect_to @goat
+  end
+
+  def destroy
+    @goat.destroy
+    redirect_to goats_path
+  end
+
+  private
+
+  def set_goat
+    @goat = Goat.find(params[:id])
+  end
+
+  def goat_params
+    params.require(:goat).permit(:name)
+  end
+end
