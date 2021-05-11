e318e51b57bc2c9b403b6703bacc19c39b059041
Felipe Santos <fecsantos@hotmail.com>
2021-05-11 20:59:48 +0100

    Increased lenght validation in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :goat
 
-  validates :comment, presence: true, length: { in: 5..30 }
+  validates :comment, presence: true, length: { in: 5..300 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
 end
