9a5c533367164b7c870841c0ff09e8a4e9bd007d
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-12 19:56:55 +0100

    updated routes and corrected rwview name to reviews (controller)

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, except: [:index, :new, :create]
   resources :goats do
-    resources :reservations
+    resources :reservations, except: [:destroy, :update]
     resources :reviews, only: [:create]
   end
+  resources :reservations, only: [:destroy, :update]
+  resources :reviews, only: [:destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
