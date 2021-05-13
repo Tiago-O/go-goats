c014ee619181495f10fae255a9ddf8ab90ffb60d
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-13 20:15:44 +0100

    deleted validation availability date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :goat
 
-  validates :date, presence: true, availability: true
+  validates :date, presence: true
   validates :location, presence: true
 end
-date e location
