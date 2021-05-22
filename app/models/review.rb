d9fbbe7904e64427f2fa75d950ec50b60211c618
Felipe Santos <fecsantos@hotmail.com>
2021-05-22 15:16:30 +0100

    goats show front end

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :goat
+  paginates_per 3
 
   validates :comment, presence: true, length: { in: 5..300 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
