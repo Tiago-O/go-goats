90351de38990fedeebd55c16589d851b558727cd
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-22 12:09:58 +0100

    algolia search override

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,17 +1,10 @@
 import places from 'places.js';
 
-const initAutocompleteGoatLocation = () => {
-  const registrationGoatInput = document.querySelector('.goat-location');
-  if (registrationGoatInput) {
-    places({ container: registrationGoatInput });
-  }
-};
-
 const initAutocomplete = () => {
-  const addressInput = document.querySelector('.home-form');
+  const addressInput = document.getElementById('location-search');
   if (addressInput) {
     places({ container: addressInput });
   }
 };
 
-export { initAutocomplete, initAutocompleteGoatLocation };
+export { initAutocomplete };
