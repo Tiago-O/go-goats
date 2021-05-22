c43f172a5cee2d49cf6c3142337f4fe51ffcdc66
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-22 10:10:12 +0100

    stops dates form overlaping

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,23 +6,12 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates :location, presence: true
 
-  validate :new_reservation_start_date_cannot_be_between_reserved_dates,
-    :new_reservation_end_date_cannot_be_between_reserved_dates,
-    :new_start_date_cant_be_in_past,
+  validate :new_start_date_cant_be_in_past,
     :new_end_date_cant_be_in_past,
-    :end_date_cant_be_before_start_date
-
-  def new_reservation_start_date_cannot_be_between_reserved_dates
-    if Reservation.exists?(goat: goat, start_date: (start_date..end_date))
-      errors.add(:start_date, "is already picked")
-    end
-  end
-
-  def new_reservation_end_date_cannot_be_between_reserved_dates
-    if Reservation.exists?(goat: goat, end_date: (start_date..end_date))
-      errors.add(:end_date, "is already picked")
-    end
-  end
+    :end_date_cant_be_before_start_date,
+    # :new_reservation_start_date_cannot_be_between_reserved_dates,
+    # :new_reservation_end_date_cannot_be_between_reserved_dates,
+    :dates_dont_overlap
 
   def new_start_date_cant_be_in_past
     if start_date < Date.today
@@ -41,4 +30,26 @@ class Reservation < ApplicationRecord
       errors.add(:end_date, "needs be after the start date")
     end
   end
+
+  def dates_dont_overlap
+    sql = "goat_id=:goat_id AND (start_date, end_date) OVERLAPS (:start, :end)"
+    if Reservation.where(sql, start: start_date, end: end_date, goat_id: goat_id).any?
+      errors.add(:start_date, "is already picked")
+      errors.add(:end_date, "is already picked")
+    end
+  end
+
+  # made redundant by the dates_dont_overlap:
+
+  # def new_reservation_start_date_cannot_be_between_reserved_dates
+  #   if Reservation.exists?(goat: goat, start_date: (start_date..end_date))
+  #     errors.add(:start_date, "is already picked")
+  #   end
+  # end
+
+  # def new_reservation_end_date_cannot_be_between_reserved_dates
+  #   if Reservation.exists?(goat: goat, end_date: (start_date..end_date))
+  #     errors.add(:end_date, "is already picked")
+  #   end
+  # end
 end
