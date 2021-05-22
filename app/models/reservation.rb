fd922c5a0e12c7d603626bf6bab434daab50c50c
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-22 16:15:45 +0100

    calendar impremented

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,24 +14,28 @@ class Reservation < ApplicationRecord
     :dates_dont_overlap
 
   def new_start_date_cant_be_in_past
+    return unless start_date
     if start_date < Date.today
       errors.add(:start_date, "can't be in the past")
     end
   end
 
   def new_end_date_cant_be_in_past
+    return unless end_date
     if end_date < Date.today
       errors.add(:end_date, "can't be in the past")
     end
   end
 
   def end_date_cant_be_before_start_date
+    return unless start_date
     if start_date > end_date
       errors.add(:end_date, "needs be after the start date")
     end
   end
 
   def dates_dont_overlap
+    return unless start_date && end_date
     sql = "goat_id=:goat_id AND (start_date, end_date) OVERLAPS (:start, :end)"
     if Reservation.where(sql, start: start_date, end: end_date, goat_id: goat_id).any?
       errors.add(:start_date, "is already picked")
